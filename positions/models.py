f6d7bb379d00204dc8e559171da873c8ddf36252
nicholas-martino <nicholas.martino@hotmail.com>
2021-07-07 18:58:27 -0700

    default end_date parameter

--- positions/models.py
+++ positions/models.py
@@ -1,10 +1,11 @@
 from django.db import models
+from django.utils import timezone
 from datetime import datetime
 
 class Position(models.Model):
 	title = models.CharField(max_length=100)
 	start_date = models.DateField()
-	end_date = models.DateField(default=datetime.now())
+	end_date = models.DateField(default=timezone.now())
 	institution = models.CharField(max_length=500)
 	description = models.TextField(default=' ')
 	hours = models.IntegerField(null=True, blank=True)
