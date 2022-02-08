a531e68b26aa3fe2ae6985a74e0682843bcc8450
Nicholas Martino <nicholasmartino@host219-99.vpn.ubc.ca>
2022-02-08 15:49:27 -0800

    Resume updates

--- static/js/summary.js
+++ static/js/summary.js
@@ -1,3 +1,3 @@
-var summary = "Data Scientist with 5+ years of experience in building data-driven applications. Proficient in advanced analytics, predictive modelling, data processing, data mining, and geospatial analysis. Established ability in employing algorithmic thinking to solve business and spatial problems across scales for a diversity of industries.";
+var summary = "5+ years of experience in building data-driven applications. Proficient in advanced analytics, predictive modelling, data processing, data mining, and geospatial analysis. Established ability in employing algorithmic thinking to solve business and spatial problems across scales for a diversity of industries.";
 document.getElementById("summary1").innerHTML = summary;
 document.getElementById("summary2").innerHTML = summary;
