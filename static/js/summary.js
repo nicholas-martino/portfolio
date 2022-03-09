fbd6cb22bfe39eff051052b4db60939118ce667b
Nicholas Martino <nicholasmartino@host209-99.vpn.ubc.ca>
2022-03-08 19:40:53 -0800

    Summary change

--- static/js/summary.js
+++ static/js/summary.js
@@ -1,3 +1,3 @@
-var summary = "5+ years of experience in building data-driven applications. Proficient in advanced analytics, predictive modelling, data processing, data mining, and geospatial analysis. Established ability in employing algorithmic thinking to solve business and spatial problems across scales for a diversity of industries.";
+var summary = "Developer with 5+ years of experience in building data-driven models and applications. Proficient in computational design, geospatial analysis, web development, predictive analytics, data mining and databases.";
 document.getElementById("summary1").innerHTML = summary;
 document.getElementById("summary2").innerHTML = summary;
